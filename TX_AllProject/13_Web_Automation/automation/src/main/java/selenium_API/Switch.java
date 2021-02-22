943ac19ce77a6813b040227e559443d26d9f3609
zhangchang <zhangchanget@126.com>
2021-02-22 17:33:48 +0800

    java自动化2021.02.22

--- TX_AllProject/13_Web_Automation/automation/src/main/java/selenium_API/Switch.java
+++ TX_AllProject/13_Web_Automation/automation/src/main/java/selenium_API/Switch.java
@@ -1,5 +1,8 @@
 package selenium_API;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,10 +14,37 @@ import org.testng.annotations.Test;
 
 public class Switch {
 
+    WebDriver driver;
 
-//    @BeforeMethod
-//    public void beforeTest
+    @BeforeMethod
+    public void beforeTest(){
+        System.out.println("start");
+        driver = new ChromeDriver();
+    }
+
 //    @Test
-//    @AfterMethod
+    public void test1(){
+        driver.get("https://www.cnblogs.com/Owen-ET/p/9636215.html");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("mac 安装MySQL")).click();
+        String handel1 = driver.getWindowHandle();
+        for (String handel: driver.getWindowHandles()){
+            if (handel.equals(handel1)){
+                driver.switchTo().window(handel);
+                break;
+            }
+            System.out.println(handel);
+        }
+    }
+
+    @Test
+    public void test2(){
+        driver.get("");
+    }
+
+    @AfterMethod
+    public void afterTest(){
+        System.out.println("stop");
+    }
 
 }
